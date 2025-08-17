2962055846f7f034b4151db42ed1427878905241
Jessica Nguyen <jn3677@rit.edu>
2025-08-16 21:26:46 -0400

    Scaffold AppInfo

---
+++ Models/AppInfo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppLock.Models
+{
+    internal class AppInfo
+    {
+        public string Name { get; set; }
+        public string ExecutablePath { get; set; }
+        public string ProcessName { get; set; }
+        public bool IsProtected { get; set; }
+        public LockMode mode { get; set; }
+
+        public AppInfo(string name, string executablePath, string processName, bool isProtected, LockMode mode)
+        {
+            Name = name;
+            ExecutablePath = executablePath;
+            ProcessName = processName;
+            IsProtected = isProtected;
+            this.mode = mode;
+        }
+
+
+    }
+}
