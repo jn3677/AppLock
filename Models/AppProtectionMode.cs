eb49b5b0cb063a8ab6ea58fd5e805c457ac63879
Jessica Nguyen <jn3677@rit.edu>
2025-08-21 17:59:26 -0400

    Added more files and filled out AppProtectionMode enum

---
+++ Models/AppProtectionMode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppLock.Models
+{
+    /// <summary>
+    /// Deines how an App should be protexted when lauched
+    /// Maps to LockMode
+    /// </summary>
+    public enum AppProtectionMode
+    {
+        None = 0,
+        Lock = 1,
+        IsolatedInstance = 2,
+        Sandbox = 3
+
+    }
+}
