dc746ee0a1a47c99e5c16f4e325bc419b11b3f53
Jessica Nguyen <jn3677@rit.edu>
2025-08-21 16:10:44 -0400

    WindowsHelloService functions implemented

--- Services/WindowsHelloService.cs
+++ Services/WindowsHelloService.cs
@@ -4,24 +4,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Security.Credentials.UI;
 
 namespace AppLock.Services
 {
     internal class WindowsHelloService : IAuthService
     {
         public string Name => "Windows Hello";
-        
 
+        /// <summary>
+        /// Checks if Windows Hello is available for authentication on device.
+        /// </summary>
+        /// <returns> bool </returns>
         public bool IsAvailable()
         {
-            //TODO: 
-            return true;
+            var availability = UserConsentVerifier.CheckAvailabilityAsync().GetAwaiter().GetResult();
+            return availability == UserConsentVerifierAvailability.Available;
         }
 
+        /// <summary>
+        /// Attempts to authenticate the user using Windows Hello.
+        /// </summary>
+        /// <returns> bool: User authenticated </returns>
         public bool Authenticate()
         {
-            //TODO
-            return true;
+            var result = UserConsentVerifier.RequestVerificationAsync("Authenticate to unlock App").GetAwaiter().GetResult();
+            return result == UserConsentVerificationResult.Verified;
         }
 
 
