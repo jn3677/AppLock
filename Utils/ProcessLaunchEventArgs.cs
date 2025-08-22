31b54f04d3676ab6301c1185da1bb29d588d3e06
Jessica Nguyen <jn3677@rit.edu>
2025-08-22 13:21:35 -0400

    Added ProcessLaunchEventArgs

---
+++ Utils/ProcessLaunchEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using AppLock.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppLock.Utils
+{
+    public class ProcessLaunchEventArgs
+    {
+        public ProcessInfo ProcessInfo { get; }
+        public bool ShouldBlock { get; set; } = false;
+        public string BlockReason { get; set; } 
+
+        public ProcessLaunchEventArgs(ProcessInfo processInfo)
+        {
+            ProcessInfo = processInfo ?? throw new ArgumentNullException(nameof(processInfo), "Process info cannot be null");
+        }
+    }
+}
