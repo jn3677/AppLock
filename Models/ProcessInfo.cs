3fa74ab57f05d7f3861a3b53c2a87d04497725e4
Jessica Nguyen <jn3677@rit.edu>
2025-08-22 13:15:41 -0400

    Added ProcessInfo

---
+++ Models/ProcessInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppLock.Models
+{
+    /// <summary>
+    /// Info about a specific process that was launched or is being tracked
+    /// </summary>
+    public class ProcessInfo
+    {
+        public string ProcessName { get; set; }
+        public string ExecutablePath { get; set; }
+        public string CommandLine { get; set; }
+        public int ProcessId { get; set; }
+        public DateTime LaunchTime { get; set; }
+        public string ParentProcessName { get; set; }
+        public int ParentProcessId { get; set; }
+        public ProcessInfo()
+        {
+            LaunchTime = DateTime.Now;  
+        }
+
+        public override string ToString()
+        {
+            return $"{ProcessName} (PID: {ProcessId}) - {ExecutablePath}";
+        }
+    }
+}
---
+++ Services/ProcessMonitorService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppLock.Models;
+
+namespace AppLock.Services
+{
+    public class ProcessMonitorService
+    {
+        public event EventHandler<AppInfo> ProcessLaunched;
+
+
+    }
+}
